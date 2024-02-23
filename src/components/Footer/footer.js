e628e495f038177365f7190c32576a912ab48c94
Oloruntobi <oloruntobialademehin@gmail.com>
2024-02-23 16:24:53 +0100

    second commit

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
-    const date = new Date();
-    const currentYear = date.getFullYear();
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  // const currentYearMonth = date.getMonth();
+  // const currentYearDay = date.getDay();
 
   return (
     <footer className="footer">
-        copyright &#169; {currentYear} Samson Obong. All right reserved.
+      copyright &#169; {currentYear} Samson Obong. All right reserved.
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
